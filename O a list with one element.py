ef7c4d06d70839c8899b1cf973caea313c76f95b
chekuri venkateswarlu <chekurivenkateswarlu7@gmail.com>
2024-10-07 13:26:21 +0530

    Add files via upload

---
+++ O a list with one element.py
@@ -0,0 +1,20 @@
+empty_list=[]
+print("test case 1: an empty")
+print("input",empty_list)
+print("expected output",empty_list)
+print()
+single_element_list=[2]
+print("test case2:list with eleemnts")
+print("input",single_element_list)
+print("expected output",single_element_list)
+print()
+identical_elements_list=[7,7,7,7]
+print("test case 3: a list with all identical elements")
+print("input:",identical_elements_list)
+print("expected output:",identical_elements_list)
+print()
+negative_numbers_list=[-1,-2,-3,-4]
+print("test case 4: a list with negative numbers")
+print("input",negative_numbers_list)
+print("expected output",negative_numbers_list)
+print()
